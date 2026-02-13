fec87366159f43de1ebf3cada0c0c56d28680899
Miracle Nwabuisi <Thebluelogic@gmail.com>
2026-02-13 17:43:47 +0100

    updated endpoint (temp)

--- backend/src/routes/employerRoutes.js
+++ backend/src/routes/employerRoutes.js
@@ -5,30 +5,14 @@ import {
   updateEmployerProfile,
   selectEmployerPlan,
   getEmployerDashboard,
-  upload,
 } from "../controllers/employerController.js";
 
 const router = express.Router();
-// signup
+
 router.post("/signup", registerEmployer);
-
-// verify  
 router.post("/verify", verifyEmployerEmail);
-
-// profile
-router.put(
-  "/update/:id",
-  upload.fields([
-    { name: "logo", maxCount: 1 },
-    { name: "gallery", maxCount: 10 },
-  ]),
-  updateEmployerProfile
-);
-
-// plans
+router.put("/update/:id", updateEmployerProfile);
 router.post("/plan/:id", selectEmployerPlan);
-
-// dashboard
 router.get("/dashboard/:id", getEmployerDashboard);
 
 export default router;
